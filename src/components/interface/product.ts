6ca324d58e781184ab655ca27de8681427d5e897
Artem Pasko <guramibox777@gmail.com>
2022-12-19 12:34:42 +0300

    refactor: change interface name to lowercase

--- src/components/interface/product.ts
+++ src/components/interface/product.ts
@@ -11,3 +11,10 @@ export interface Product {
   thumbnail: string;
   images: string[];
 }
+
+export interface ProductResponse {
+  products: Product[];
+  total: number;
+  skip: number;
+  limit: number;
+}
