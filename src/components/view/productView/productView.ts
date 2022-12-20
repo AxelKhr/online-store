a74de031aeadf73807336f5727cf31ad8a9e4af7
Aliaksandr Khramau <axelkhr@gmail.com>
2022-12-20 15:07:16 +0100

    feat: create basic structure for product-view page

--- src/components/view/productView/productView.ts
+++ src/components/view/productView/productView.ts
@@ -1,11 +1,21 @@
+import "./style.scss";
 import { AbstractView } from "../abstractView";
 
 export class ProductView extends AbstractView {
 
     async getView(): Promise<HTMLElement> {
-        let content = document.createElement('section') as HTMLElement;
-        content.classList.add('product-page');
-        content.innerHTML = `<span>Product page</span>`;
+        const content = document.createElement('section') as HTMLElement;
+        content.classList.add('content__product-page', 'product-page');
+        const path = document.createElement('div');
+        path.classList.add('product-page__path');
+        const box = document.createElement('div');
+        box.classList.add('product-page__box');
+        const view = document.createElement('div');
+        view.classList.add('product-page__view');
+        const control = document.createElement('div');
+        control.classList.add('product-page__control');
+        box.append(view, control);
+        content.append(path, box);
         return content;
     }
 }
