63a14eaa41d5afd098a13f0665ffb28ab01e3c29
Aliaksandr Khramau <axelkhr@gmail.com>
2022-12-20 19:47:36 +0100

    feat: create draw function for product-view

--- src/components/view/productView/productView.ts
+++ src/components/view/productView/productView.ts
@@ -1,5 +1,6 @@
 import "./style.scss";
 import { AbstractView } from "../abstractView";
+import { Product } from "../../interface/product";
 
 export class ProductView extends AbstractView {
 
@@ -15,7 +16,57 @@ export class ProductView extends AbstractView {
         const control = document.createElement('div');
         control.classList.add('product-page__control');
         box.append(view, control);
-        content.append(path, box);
+        const detailing = document.createElement('div');
+        detailing.classList.add('product-page__detailing');
+        content.append(path, box, detailing);
         return content;
+    }
+
+    drawView(data: Product) {
+
+        const createElemP = (classNames: string[], textContent?: string): HTMLParagraphElement => {
+            const par = document.createElement('p');
+            par.classList.add(...classNames);
+            if (textContent) {
+                par.textContent = textContent;
+            }
+            return par;
+        }
+
+        const path = document.querySelector('.product-page__path') as HTMLElement;
+        path.textContent = 'Path';
+
+        const view = document.querySelector('.product-page__view') as HTMLElement;
+
+        const image = document.createElement('div');
+        image.classList.add('view__image');
+        image.style.backgroundImage = `url(${data.images[0]})`;
+        view.append(image);
+
+        const control = document.querySelector('.product-page__control') as HTMLElement;
+
+        const buttonAdd = document.createElement('button');
+        buttonAdd.classList.add('control__button-add');
+        buttonAdd.textContent = 'ADD';
+        const buttonBuy = document.createElement('button');
+        buttonBuy.classList.add('control__button-buy');
+        buttonBuy.textContent = 'BUY';
+
+        control.append(
+            createElemP(['control__title'], data.title),
+            createElemP(['control__brand'], data.brand),
+            createElemP(['control__rating'], `Rating: ${data.rating}`),
+            createElemP(['control__discount'], `Discount: ${data.discountPercentage}%`),
+            createElemP(['control__stock'], `Stock: ${data.stock}`),
+            createElemP(['control__price'], `Price: ${data.price}`),
+            buttonAdd, buttonBuy);
+
+        const detailing = document.querySelector('.product-page__detailing') as HTMLElement;
+        detailing.append(
+            createElemP(['detailing__title'], 'Detailing'),
+            createElemP(['detailing__item'], `Category: ${data.category}`),
+            createElemP(['detailing__item'], `Brand: ${data.brand}`),
+            createElemP(['detailing__item'], `Description: ${data.description}`)
+        );
     }
 }
