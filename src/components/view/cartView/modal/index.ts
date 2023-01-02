4599b18dc3d7d004726d18a4077664f3a66463d3
Artem Pasko <guramibox777@gmail.com>
2023-01-02 23:12:54 +0300

    feat: add modal pattern

--- src/components/view/cartView/modal/index.ts
+++ src/components/view/cartView/modal/index.ts
@@ -7,13 +7,13 @@ export function getModal(): HTMLElement {
     const modalShadow = document.querySelector('.modal__shadow');
     modalShadow?.addEventListener('click', closeModal);
     modal.innerHTML =`
-    <div class="modal__window">
-        <input class="modal__input" type="text" placeholder="Name">
-        <input class="modal__input" type="text" placeholder="Phone number">
-        <input class="modal__input" type="text" placeholder="Address">
-        <input class="modal__input" type="text" placeholder="E-mail">
+    <form class="modal__window">
+        <input class="modal__input" type="text" pattern="^\\w{3,}\\s+\\w{3,}$" required placeholder="Name">
+        <input class="modal__input" type="tel" placeholder="Phone number" pattern="^[\+]\\d{9,}$" required>
+        <input class="modal__input" type="text" placeholder="Address" pattern="^\\w{5,}\\s+\\w{5,}\\s+\\w{5,}$" required>
+        <input class="modal__input" type="email" placeholder="E-mail" pattern="^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$" required>
         <button data-hystclose class="modal__confirm">Confirm</button>
-    </div>`;
+    </form>`;
     return modal;
 }
 
